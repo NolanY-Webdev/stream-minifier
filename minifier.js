7664255cb262addef13c039f86c4f6cecdd703e2
SkinnyFatMan <SkinnyFatMan@gmail.com>
2015-10-20 10:51:07 -1000

    seems to be working

--- minifier.js
+++ minifier.js
@@ -2,6 +2,25 @@
 
 var fs = require('fs');
 var util = require('util');
-var argv = require('../')(process.argv.slice(2));
+var inherits = require('util').inherits;
+var Transform = require('util').Transform;
+var argv = require('minimist')(process.argv.slice(2));
+
+var readableStdin = fs.createReadStream(argv.input);
+
+var writeableStdout = fs.createWriteStream(argv.output);
+
+function Transpiler() {
+  Transform.call(this);
+}
+
+inherits(Transpiler, Transform);
+
+Transpiler.prototype._transform = function(chunk, encoding, done) {
+  chunk = chunk.toString().replace(/(\r\n|\n|\r|\s)/gm, "");
+  this.push(chunk);
+  done();
+};
 
 
+readableStdin.pipe(new Transpiler).pipe(writeableStdout);
